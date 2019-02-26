4d2ce7d19bf1f0e4a06d3fca5b0d7ea93d145386
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2019-02-26 07:51:50 -0800

    clang-format header files

--- src/physics_2d.h
+++ src/physics_2d.h
@@ -3,78 +3,68 @@
 
 namespace phys {
 
-    struct Vec2f {
-        float x;
-        float y;
+struct Vec2f {
+  float x;
+  float y;
 
-        Vec2f inline operator=(Vec2f a) {
-            x = a.x;
-            y = a.y;
-            return a;
-        }
+  Vec2f inline operator=(Vec2f a) {
+    x = a.x;
+    y = a.y;
+    return a;
+  }
 
-        Vec2f inline operator+(Vec2f a) {
-            return { a.x + x, a.y + y };
-        }
+  Vec2f inline operator+(Vec2f a) { return {a.x + x, a.y + y}; }
 
-        const Vec2f inline operator-(Vec2f a) const {
-            return { a.x - x, a.y - y };
-        }
+  const Vec2f inline operator-(Vec2f a) const { return {a.x - x, a.y - y}; }
 
-        Vec2f inline operator-(Vec2f a) {
-            return { a.x - x, a.y - y };
-        }
+  Vec2f inline operator-(Vec2f a) { return {a.x - x, a.y - y}; }
 
+  float inline operator*(Vec2f a) { return a.x * x + a.y * y; }
+};
 
-        float inline operator*(Vec2f a) {
-            return a.x*x + a.y*y;
-        }
-    };
+struct AABB {
+  Vec2f min;
+  Vec2f max;
+};
 
-    struct AABB {
-        Vec2f min;
-        Vec2f max;
-    };
+struct Circle {
+  float radius;
+  Vec2f pos;
+};
 
-    struct Circle {
-        float radius;
-        Vec2f pos;
-    };
+struct PhysObject {
+  Circle circle;
+  Vec2f velocity;
+  float restitution;  // How bouncy this object is in collisions
+  float mass;
+};
 
-    struct PhysObject {
-        Circle circle;
-        Vec2f velocity;
-        float restitution; //How bouncy this object is in collisions
-        float mass;
+class Simulator {
+ public:
+  void add(PhysObject object);
+  void step(float dt);
+  std::vector<PhysObject> objects();
 
-    };
+ private:
+  std::vector<PhysObject> m_objects{};
+};
 
-    class Simulator {
-    public:
-        void add(PhysObject object);
-        void step(float dt);
-        std::vector<PhysObject> objects();
-    private:
-        std::vector<PhysObject> m_objects{};
-    };
-
-    Vec2f inline operator*(Vec2f b, float a) {
-        return {a*b.x, a*b.y };
-    }
-
-    Vec2f inline operator*(float a, Vec2f b) {
-        return {a*b.x, a*b.y };
-    }
-
-
-    float SquaredDistance(Vec2f a, Vec2f b);
-    float Distance(Vec2f a, Vec2f b);
-
-    bool AABBvsAABB(AABB a, AABB b);
-    bool CirclevsCircle(Circle a, Circle b);
-
-    Vec2f collisionNormal(Circle A, Circle B);
-
-    void ResolveCollision(const PhysObject &A, const PhysObject &B);
-
+Vec2f inline operator*(Vec2f b, float a) {
+  return {a * b.x, a * b.y};
 }
+
+Vec2f inline operator*(float a, Vec2f b) {
+  return {a * b.x, a * b.y};
+}
+
+float SquaredDistance(Vec2f a, Vec2f b);
+float Distance(Vec2f a, Vec2f b);
+
+bool AABBvsAABB(AABB a, AABB b);
+bool CirclevsCircle(Circle a, Circle b);
+
+Vec2f collisionNormal(Circle A, Circle B);
+
+void ResolveCollision(const PhysObject& A, const PhysObject& B);
+
+}  // namespace phys
--- src/renderer_opengl.h
+++ src/renderer_opengl.h
@@ -1,19 +1,17 @@
 #pragma once
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include "physics_2d.h"
 
 class OpenGLRenderer {
-public:
-    void init();
-    void clear();
-    void drawCircle(phys::Circle circle);
-    void finishRendering();
-    bool shouldClose();
-private:
-    GLFWwindow *mWindow;
-    GLuint m_shader_program_flat{};
+ public:
+  void init();
+  void clear();
+  void drawCircle(phys::Circle circle);
+  void finishRendering();
+  bool shouldClose();
+
+ private:
+  GLFWwindow* mWindow;
+  GLuint m_shader_program_flat{};
 };
-
-
-
